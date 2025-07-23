5c372e5e016d746ba8a51dc77354d5dcd053def5
aidanbeck <aidanmbeck@outlook.com>
2025-07-23 07:52:42 -0500

    options component prop

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,12 +4,21 @@ import Options from './Options';
 
 import '../styles/play.css';
 
+const choices = [
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, totam!",
+  "Lorem ipsum dolor sit.",
+  "Lorem ipsum dolor sit amet consectetur.",
+  "Lorem ipsum dolor sit amet.",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, nemo animi."
+]
+
 function Game() {
   return (
     <div id="playScreen">
       <h1><Link id="backToTitle" to='/'>Old Cove</Link></h1>
       <Information />
-      <Options />
+      <Options options={choices}/>
     </div>
   )
 }
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,13 +1,14 @@
-function Options() {
-    return (
-        <div id="options">
-        <div className="option">Option 1</div>
-        <div className="option">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem nam distinctio harum dolorem, veniam delectus vero ducimus culpa cum obcaecati ullam sint ut corrupti error amet consectetur alias assumenda praesentium?</div>
+function Options(props) {
 
-        <div className="option">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias magni illo numquam voluptates repellat, libero id quas dicta aut cupiditate quibusdam! Hic quas rem cum aut rerum sequi odit atque.</div>
-        <div className="option"><p>test</p></div>
-      </div>
-    );
+  let optionsJSX = props.options.map( (option, index) => {
+    return <div key={index} className="option">{option}</div>
+  });
+
+  return (
+    <div id="options">
+      {optionsJSX}
+    </div>
+  );
 }
 
 export default Options;
