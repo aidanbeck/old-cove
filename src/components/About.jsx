845263e3c90c1b43138befb6b1c6ae19eb7c89c8
aidanbeck <aidanmbeck@outlook.com>
2025-07-23 13:53:57 -0500

    links paintings to their nga.gov pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,16 +26,16 @@ function Title () {
                 <caption>Painting Credits</caption>
                 <tbody>
                     <tr>
-                        <td class="painting"><img src={image1}/></td>
+                        <td class="painting"><a href="https://www.nga.gov/artworks/50729-much-resounding-sea"><img src={image1}/></a></td>
                         <td><strong>The Much Resounding Sea</strong><br/><em>Thomas Moran</em> 1884</td>
 
                     </tr>
                     <tr>
-                        <td class="painting"><img src={image2} id="calmSea"/></td>
+                        <td class="painting"><a href="https://www.nga.gov/artworks/66408-calm-sea"><img src={image2} id="calmSea"/></a></td>
                         <td><strong>Calm Sea</strong><br/><em>Gustave Courbet</em><br/>1866</td>
                     </tr>
                     <tr>
-                        <td class="painting"><img src={image3}/></td>
+                        <td class="painting"><a href="https://www.nga.gov/artworks/56574-beach-beverly"><img src={image3}/></a></td>
                         <td><strong>Beach at Beverly</strong><br/><em>John Frederick Kensett</em><br/>Between 1869 and 1872</td>
                     </tr>
                 </tbody>
