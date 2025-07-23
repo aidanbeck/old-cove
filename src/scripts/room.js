cc905b1e836648385a4ef0d882f81c0dd1930817
aidanbeck <aidanmbeck@outlook.com>
2025-07-23 15:31:56 -0500

    give items & locations

--- src/scripts/room.js
+++ src/scripts/room.js
@@ -1,4 +1,5 @@
 // to make a "give item" option, make the option link to the room it's in. Have it give an item. Make a description of the user picking it up.
+// can this edit a description of a room? can a room's description change if it has that item?
 
 class Path {
     constructor(toId, prompt, description=[], require='', giveItem='') {
@@ -11,10 +12,11 @@ class Path {
 }
 
 class Room {
-    constructor(title = "untitled", description = ['blank'], paths = []) {
+    constructor(title = "untitled", description = ['blank'], paths = [], giveLocation='') {
         this.title = title; // key & location text
         this.description = description; // array of paragraphs
         this.paths = paths; //array of path objects
+        this.giveLocation = giveLocation; // add key to location list
     }
 }
 
@@ -27,44 +29,115 @@ class World {
 
         this.currentRoom = rooms[currentId];
     }
-    moveTo(id) {
-        this.currentId = id;
-        this.currentRoom = this.rooms[id];
-        console.log(id+"abc");
-    }
+
     hasItem(item) {
         if (this.inventory.includes(item)) {
             return true;
         }
         return false;
     }
+    giveItem(item) {
+        if (this.inventory.includes(item)) {
+            return; // prevents having the same item twice. Needed?
+        }
+        this.inventory[this.inventory.length] = item;
+    }
+
+    hasLocation(location) {
+        if (this.locations.includes(location)) {
+            return true;
+        }
+        return false;
+    }
+    giveLocation(id) {
+        // this assumes id's == locations/titles!! should be fine, but beware!
+        if (this.locations.includes(id)) {
+            return; // prevents having the same location twice.
+        }
+        this.locations[this.locations.length] = id;
+    }
+
+    moveTo(id) {
+        this.currentId = id;
+        this.currentRoom = this.rooms[id];
+        this.giveLocation(id);
+    }
     choosePath(index) {
         let path = this.currentRoom.paths[index];
 
+        // Get Conditionals
+        let playerHasNeededItem = path.require == '' || this.hasItem(path.require);
+        let playerHasGivenItem = this.hasItem(path.giveItem);
+        let pathHasItemToGive = path.giveItem != '';
 
-        let hasNeededItem = path.require == '' || this.hasItem(path.require);
-        let hasGivenItem = this.hasItem(path.giveItem);
-
-        if (hasNeededItem && !hasGivenItem) {
+        if (playerHasNeededItem && !playerHasGivenItem) {
+            if (pathHasItemToGive) { this.giveItem(path.giveItem); }
             this.moveTo(path.toId);
         }
     }
+    
+    
 }
 
-let paths = [
-    new Path("lighthouse", "go to lighthouse", '', 'axe'),
-    new Path("town", "go to town"),
-    new Path("cave", "go to cave")
+
+// Create World Data
+let beachPaths = [
+    new Path("lighthouse", "go to lighthouse"),
+    new Path("town", "go to the town"),
+    new Path("cave", "go to the cave")
+]
+
+let townPaths = [
+    new Path ("sandy beach", "return to the beach.")
+]
+
+let lighthousePaths = [
+    new Path("sandy beach", "return to the beach"),
+    new Path("lighthouse", "take key", [], '', 'key'),
+    new Path("the light", "destroy the barrel.", ['you break the barrel with the axe'], 'axe')
+]
+
+let lightPaths = [
+    new Path("lighthouse", "climb back down")
+]
+
+let cavePaths = [
+    new Path("sandy beach", "exit the cave"),
+    new Path ("storage room", "open locked door", [], 'key')
+]
+
+let storagePaths = [
+    new Path("cave", "return to the cave"),
+    new Path("storage room", "take axe", [], '', 'axe')
 ]
 
 let rooms = {
-    "sandy beach": new Room("sandy beach", "the beach is sandy", paths),
-    "lighthouse": new Room("lighthouse", "the lighthouse shines bright")
+    "sandy beach": new Room("sandy beach", "the beach is sandy", beachPaths),
+    "town": new Room("town", "everyone's moved on now.", townPaths),
+    "lighthouse": new Room("lighthouse", "the lighthouse shines bright. There is a barrel blocking the way in.", lighthousePaths),
+    "cave": new Room("cave", "It's hard to see in here", cavePaths),
+    "storage room": new Room('storage room', "no one's been here for a long time", storagePaths),
+    "the light": new Room ('the light', "the view is beautiful.", lightPaths)
 }
 
-let gameWorld = new World(rooms, "sandy beach", [], ['axe']);
+let gameWorld = new World(rooms, "sandy beach");
 
 
 // Testing
-gameWorld.choosePath(0);
-console.log(gameWorld.currentRoom);
+
+function logStatus() {
+    console.log(gameWorld.currentRoom.description, gameWorld.inventory);
+}
+logStatus();
+gameWorld.choosePath(1); logStatus();
+gameWorld.choosePath(0); logStatus();
+gameWorld.choosePath(0); logStatus();
+gameWorld.choosePath(1); logStatus();
+gameWorld.choosePath(0); logStatus();
+gameWorld.choosePath(2); logStatus();
+gameWorld.choosePath(1); logStatus();
+gameWorld.choosePath(1); logStatus();
+gameWorld.choosePath(0); logStatus();
+gameWorld.choosePath(0); logStatus();
+gameWorld.choosePath(0); logStatus();
+gameWorld.choosePath(2); logStatus();
