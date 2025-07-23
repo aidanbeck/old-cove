1d7cfe8d3b5d4da5f98ad1831973ca2315593269
aidanbeck <aidanmbeck@outlook.com>
2025-07-23 16:47:09 -0500

    clickable interactable options

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,20 +1,28 @@
 import { Outlet, Link } from "react-router-dom";
+import { useState } from 'react';
 import Information from './Information';
 import Options from './Options';
-
 import world from '../scripts/room';
-
 import '../styles/play.css';
 
 function Game() {
 
-  let paths = world.currentRoom.paths;
+  const [worldState, setWorldData] = useState(world);
+
+  const handleChange = (e) => {
+
+    // This is a little hacky, could I somehow return just what changes? or have choosePath() change the state of worldData instead of world?
+    world.choosePath(e);
+    setWorldData( (prevData) => ({...world }) );
+  }
+
+  let paths = worldState.currentRoom.paths;
 
   return (
     <div id="playScreen">
       <h1><Link id="backToTitle" to='/'>Old Cove</Link></h1>
-      <Information world={world}/>
-      <Options options={paths}/>
+      <Information world={worldState}/>
+      <Options options={paths} handleChange={handleChange}/>
     </div>
   )
 }
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,7 +1,7 @@
 function Options(props) {
 
   let optionsJSX = props.options.map( (option, index) => {
-    return <div key={index} className="option">{option.prompt}</div>
+    return <div key={index} className="option" onClick={() => props.handleChange(index)}>{option.prompt}</div>
   });
 
   return (
