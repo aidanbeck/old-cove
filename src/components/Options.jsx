573eb485920a6793cfb8db136ed7264e24746b35
aidanbeck <aidanmbeck@outlook.com>
2025-07-24 12:19:08 -0500

    cleans up Options.jsx

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,17 +1,18 @@
 function Options(props) {
 
-  let world = props.world;
+  let { world, handleChange } = props;
   let options = world.currentRoom.paths;
 
-  let lockedJSX = [];
-
+  let lockedJSX = []; // jsx for unselectable options.
   let optionsJSX = options.map( (option, index) => {
 
-    let optionClass = "option";
-    if (options.length % 2 !== 0 && index == 0) { //is the first option of an odd number of options
-      optionClass = "option finalOption";
+    /* This block lets css handle an odd number of options by adding the 'finalOption' class */
+    let optionClass = "option"; // Set the default class to 'option'
+    if (options.length % 2 !== 0 && index == 0) { // if this is the first of an odd number of options,
+      optionClass = "option finalOption"; // add the 'finalOption' class.
     }
 
+    /* Creates options that are crossed out */
     let playerHasGivenItem = world.inventory.includes(option.giveItem);
     let optionLimitHit = option.limit <= 0;
     if (playerHasGivenItem || optionLimitHit) { // player already has the item. cross out the element.
@@ -19,14 +20,16 @@ function Options(props) {
       return;
     }
 
+    /* Creates options that are normal */
     let playerDoesntNeedItem = option.require == '';
-    if (playerDoesntNeedItem) { // normal path
-      return <div key={index} className={optionClass} onClick={() => props.handleChange("option", index)}>{option.prompt}</div>
+    if (playerDoesntNeedItem) {
+      return <div key={index} className={optionClass} onClick={() => handleChange("option", index)}>{option.prompt}</div>
     }
 
+    /* Creates options with requirements. */
     let playerHasNeededItem = world.inventory.includes(option.require);
     if (playerHasNeededItem) { 
-      return <div key={index} className={optionClass} onClick={() => props.handleChange("option", index)}><span>use {option.require.string}</span><hr/> {option.prompt}</div>;
+      return <div key={index} className={optionClass} onClick={() => handleChange("option", index)}><span>use {option.require.string}</span><hr/> {option.prompt}</div>;
     } else { // player doesn't have a required item, they should come back.
       lockedJSX[lockedJSX.length] = <div key={index} className={optionClass + " locked"}><span>Requires {option.require.string}</span><hr/> {option.prompt}</div>;
       return;
