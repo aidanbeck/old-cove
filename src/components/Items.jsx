e918fa72f2157cd8b2668c6372b48deb5ba6b78b
aidanbeck <aidanmbeck@outlook.com>
2025-08-30 10:45:53 -0500

    implement empty path culling & item descriptions

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,19 +1,19 @@
 function Items(props) {
 
-    let { items, paragraphs, handleChange } = props;
+    let { items, inventory, paragraphs, handleChange } = props;
 
-    // let itemsJSX = items.map( (item, index) => {
+    let itemsJSX = inventory.map( (item, index) => {
 
-    //     if (item.paragraphs == paragraphs) { // This compares the item's description to the room's description to determine if it is the current item. Not great!
-    //         return <div key={index} className="item currentItem" onClick={() => handleChange("item", item)}>{item.name}</div>
-    //     }
+        if (item.paragraphs == paragraphs) { // This compares the item's description to the room's description to determine if it is the current item. Not great!
+            return <div key={index} className="item currentItem" onClick={() => handleChange("item", item)}>{items[item].name}</div>
+        }
 
-    //     return <div key={index} className="item" onClick={() => handleChange("item", item)}>{item.name}</div>
-    // });
+        return <div key={index} className="item" onClick={() => handleChange("item", item)}>{items[item].name}</div>
+    });
 
     return (
         <div id="items">
-            {/* {itemsJSX} */}
+            {itemsJSX}
         </div>
     );
 }
