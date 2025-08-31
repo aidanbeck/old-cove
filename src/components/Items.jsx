b328f02a59f938d6b910951a8595aab06a602819
aidanbeck <aidanmbeck@outlook.com>
2025-08-31 16:30:20 -0500

    fix highlighting selected items

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -4,7 +4,10 @@ function Items(props) {
 
     let itemsJSX = inventory.map( (item, index) => {
 
-        if (item.paragraphs == paragraphs) { // This compares the item's description to the room's description to determine if it is the current item. Not great!
+        let itemDescriptionIsVisible = items[item].paragraphs[0]["default"] == paragraphs[0]; // This compares the item's description to the room's description to determine if it is the current item. Not great!
+        // ^ if paragraphs[0] is not the default paragraph, this will break. Hacky and bad, should refactor.
+
+        if (itemDescriptionIsVisible) {
             return <div key={index} className="item currentItem" onClick={() => handleChange("item", item)}>{items[item].name}</div>
         }
 
