e4d06a45bdda3110814094301276c5d9e65116e5
aidanbeck <aidanmbeck@outlook.com>
2025-11-02 21:12:19 -0600

    add back-end folder

---
+++ back-end/src/main/java/com/aidan/old_cove/OldCoveApplication.java
@@ -0,0 +1,13 @@
+package com.aidan.old_cove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldCoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldCoveApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/aidan/old_cove/OldCoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidan.old_cove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldCoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
