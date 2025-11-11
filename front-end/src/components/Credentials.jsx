626fe6889e0eafebd751df59712a0b4a84993f2c
aidanbeck <aidanmbeck@outlook.com>
2025-11-11 13:02:23 -0600

    change submit behavior

--- front-end/src/components/Credentials.jsx
+++ front-end/src/components/Credentials.jsx
@@ -2,26 +2,33 @@ import '../styles/choices.css';
 import '../styles/credentials.css';
 
 function Credentials(props) {
+
+    let { title, endPoint, error } = props;
     
     return (
         <div className="Credentials">
-            <form action="/user" method="post">
-                <div className="formTitle">Sign Up</div>
+            <form action={`/${endPoint}`} method="post" onSubmit={submit}>
+                <div className="formTitle">{title}</div>
 
                 <div className="credential">
                     <label for="username">Username</label>
-                    <input className="field" type="text" id="username" name="username"/>
+                    <input className="field" type="text" id="username" name="username" required/>
                 </div>
                 
                 <div className="credential">
                     <label for="password">Password </label>
-                    <input className="field" type="password" id="password" name="password"/>
+                    <input className="field" type="password" id="password" name="password" required/>
                 </div>
-                <div className="feedback">This is an Error!</div>
+                <div className="feedback">{error}</div>
                 <input className="option" type="submit" value="Submit"/>
             </form>
         </div>
     );
+}
+
+function submit(event) {
+    event.preventDefault();
+    alert("test!");
 }
 
 export default Credentials;
